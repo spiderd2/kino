460b65aa3198fc7795b4dc3cd6b2a89e0fc70ad6
spiderd2 <spiderd2@gmail.com>
2016-05-05 18:40:15 +0200

    logowanie, rejestracja i sesja

--- src/Sosnowiec/KinoBundle/Controller/HomePageController.php
+++ src/Sosnowiec/KinoBundle/Controller/HomePageController.php
@@ -5,19 +5,154 @@ namespace Sosnowiec\KinoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Sosnowiec\KinoBundle\Entity\Uzytkownicy;
+use Sosnowiec\KinoBundle\Modals\Login;
 
 /**
-     * @Route("/kino")
-     */
-class HomePageController extends Controller{
-    
+ * @Route("/kino")
+ */
+class HomePageController extends Controller {
+
     /**
-     * @Route("/")
+     * @Route("/", 
+     * name="sosnowiec_kino_index"
+     * )
      * 
      * @Template
      */
-    public function indexAction(){
-              
+    public function indexAction() {
+
         return array();
     }
+
+    /**
+     * @Route("/login",
+     *      name="sosnowiec_kino_login")
+     * 
+     * @Template
+     */
+    public function loginAction(Request $request) {
+
+        $session = $this->getRequest()->getSession();
+        $em = $this->getDoctrine()->getEntityManager();
+        $repository = $em->getRepository('SosnowiecKinoBundle:Uzytkownicy');
+        if ($session->has('login')) {
+            $login = $session->get('login');
+            $email = $login->getUsername();
+            $password = $login->getPassword();
+            $username = $repository->findOneBy(array(
+                'email' => $email,
+                'haslo' => $password
+            ));
+
+            if ($username) {
+                return $this->render('SosnowiecKinoBundle:HomePage:welcome.html.twig', array(
+                            'name' => $username->getImie()
+                ));
+            }
+        }
+        if ($request->getMethod() == 'POST') {
+            $session->clear();
+            $email = $request->get('email');
+            $password = $request->get('password');
+            $remember = $request->get('remember');
+
+            $username = $repository->findOneBy(array(
+                'email' => $email,
+                'haslo' => $password
+            ));
+
+            if ($username) {
+                if ($remember == 'remember-me') {
+                    $login = new Login();
+                    $login->setUsername($email);
+                    $login->setPassword($password);
+                    $session->set('login', $login);
+                }
+                return $this->render('SosnowiecKinoBundle:HomePage:welcome.html.twig', array(
+                            'name' => $username->getImie()
+                ));
+            } else {
+                if ($session->has('login')) {
+                    $login = $session->get('login');
+                    $email = $login->getUsername();
+                    $password = $login->getPassword();
+                    $username = $repository->findOneBy(array(
+                        'email' => $email,
+                        'haslo' => $password
+                    ));
+
+                    if ($username) {
+                        return $this->render('SosnowiecKinoBundle:HomePage:welcome.html.twig', array(
+                                    'name' => $username->getImie()
+                        ));
+                    }
+                }
+                return $this->render('SosnowiecKinoBundle:HomePage:login.html.twig', array(
+                            'msg' => "Podano nieprawidłowe dane"
+                ));
+            }
+        }
+        //return array();
+    }
+
+    /**
+     * @Route("/register",
+     *      name="sosnowiec_kino_register")
+     * 
+     * @Template
+     */
+    public function registerAction(Request $request) {
+        if ($request->getMethod() == 'POST') {
+            $login = $request->get('login');
+            $imie = $request->get('imie');
+            $nazwisko = $request->get('nazwisko');
+            $email = $request->get('email');
+            $haslo = $request->get('haslo');
+            $telefon = $request->get('telefon');
+
+            $user = new Uzytkownicy();
+            $user->setEmail($email);
+            $user->setHaslo($haslo);
+            $user->setImie($imie);
+            $user->setLogin($login);
+            $user->setNazwisko($nazwisko);
+            $user->setTelefon($telefon);
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $em->flush();
+
+            $redirectUrl = $this->generateUrl('sosnowiec_kino_login');
+            return $this->redirect($redirectUrl);
+            //return $this->render('SosnowiecKinoBundle:HomePage:login.html.twig');
+        }
+    }
+
+    /**
+     * @Route("/welcome",
+     *      name="sosnowiec_kino_welcome")
+     * 
+     * @Template
+     */
+    public function welcomeAction() {
+
+        return array();
+    }
+
+    /**
+     * @Route("/logout",
+     *      name="sosnowiec_kino_logout")
+     * 
+     * @Template
+     */
+    public function logoutAction() {
+        $session = $this->getRequest()->getSession();
+        $session->clear();
+
+        $redirectUrl = $this->generateUrl('sosnowiec_kino_login');
+        return $this->redirect($redirectUrl);
+    }
+
 }
---
+++ src/Sosnowiec/KinoBundle/Modals/Login.php
@@ -0,0 +1,25 @@
+<?php
+namespace Sosnowiec\KinoBundle\Modals;
+
+class Login {
+   
+    private $username;
+    private $password;
+    function getUsername() {
+        return $this->username;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function setUsername($username) {
+        $this->username = $username;
+    }
+
+    function setPassword($password) {
+        $this->password = $password;
+    }
+
+
+}
